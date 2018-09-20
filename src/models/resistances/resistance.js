021bd9e61b2cf4ece96a219b9e899850a4f5a490
Felix Steiner <felix_steiner@bluewin.ch>
2018-09-20 16:09:09 +0200

    fix(resistance): move observable modifying code to action

--- src/models/resistances/resistance.js
+++ src/models/resistances/resistance.js
@@ -1,36 +1,49 @@
-import {observable} from 'mobx';
+import { observable, action } from 'mobx';
 import ResistanceValue from './resistanceValue';
 
 export default class Resistance {
 
-	/**
-	* Values for different resistanceTypes. Only values must be observable, antibiotics and bacteria are not going 
-	* to change.
-	*/
-	@observable values = [];
+    /**
+    * Values for different resistanceTypes. Only values must be observable, antibiotics and
+    * bacteria are not going to change.
+    */
+    @observable values = [];
 
-	/**
-	* @param {Array} values				Array of values, each an object with properties type, 
-	*									value, sampleSize
-	* @param {Antibiotic} antibiotic	Antibiotic
-	* @param {Bacterium} bacterium		Bacterium
-	*/
-	constructor(values, antibiotic, bacterium) {
-		if (!Array.isArray(values)) throw new Error(`Resistance: First argument must be an Array of resistance values`);
-		if (!antibiotic) throw new Error(`Resistance: Argument antibiotic is required.`);
-		if (!bacterium) throw new Error(`Resistance: Argument bacterium is required.`);
-		values.forEach((value) => {
-			this.values.push(new ResistanceValue(value.type, value.value, value.sampleSize, value.confidenceInterval));
-		});
-		this.antibiotic = antibiotic;
-		this.bacterium = bacterium;
-	}
+    /**
+    * @param {Array} values             Array of values, each an object with properties type,
+    *                                   value, sampleSize
+    * @param {Antibiotic} antibiotic    Antibiotic
+    * @param {Bacterium} bacterium      Bacterium
+    */
+    constructor(values, antibiotic, bacterium) {
+        if (!Array.isArray(values)) {
+            throw new Error('Resistance: First argument must be an Array of resistance values');
+        }
+        if (!antibiotic) throw new Error('Resistance: Argument antibiotic is required.');
+        if (!bacterium) throw new Error('Resistance: Argument bacterium is required.');
+        values.forEach(value => this.addResistanceValue(value));
+        this.antibiotic = antibiotic;
+        this.bacterium = bacterium;
+    }
 
-	/**
-	* Returns all resistance values, sorted by precision (see resistanceTypes)
-	*/
-	getValuesByPrecision() {
-		return this.values.sort((a, b) => a.type.precision > b.type.precision ? -1 : 1);
-	}
+    /**
+     * Adds a value
+     * @private
+     */
+    @action addResistanceValue(resistanceValue) {
+        this.values.push(new ResistanceValue(
+            resistanceValue.type,
+            resistanceValue.value,
+            resistanceValue.sampleSize,
+            resistanceValue.confidenceInterval,
+        ));
+    }
+
+    /**
+    * Returns all resistance values, sorted by precision (see resistanceTypes)
+    */
+    getValuesByPrecision() {
+        return this.values.sort((a, b) => (a.type.precision > b.type.precision ? -1 : 1));
+    }
 
 }
