c61b3ba690d16b2e1fd897f25203bfdf3d64ce75
Felix Steiner <felix_steiner@bluewin.ch>
2018-09-22 12:19:01 +0200

    feat(export): export AntibioticMatrixView

--- index.js
+++ index.js
@@ -3,5 +3,13 @@ import filterTypes from './src/models/filters/filterTypes';
 import errorHandler from './src/models/errorHandler/errorHandler';
 import resistanceTypes from './src/models/resistances/resistanceTypes';
 
-export { InfectApp as default, filterTypes, errorHandler, resistanceTypes };
+// Export models. Needed in App to check if value returned by xPositions is an Antibiotic or
+// SubstanceClass
+import AntibioticMatrixView from './src/models/antibiotics/antibioticMatrixView';
+
+const models = {
+    AntibioticMatrixView,
+};
+
+export { InfectApp as default, filterTypes, errorHandler, resistanceTypes, models };
 
